c45ebd5f057b0ff5030cc978dccdf504a6f0ee1e
LENOVO <LENOVO@DESKTOP-22RO74R>
2022-04-10 01:38:04 -0700

    first commit

---
+++ Day2_introduction_to_java/src/com/cg/intro/CompileTimeInput.java
@@ -0,0 +1,26 @@
+package com.cg.intro;
+
+public class CompileTimeInput {
+
+	public static void main(String[] args) {
+	boolean b= true;
+	char c= 's';
+	byte by = 11;
+	short sh =1245;
+	int a = 1354122;
+	long l= 789654123;
+	float f= 22.55f;
+	double dbl = 7893.012;
+	System.out.println("your compile time input is");
+	System.out.println("boolean:  "+b);
+	System.out.println("char:	"+c);
+	System.out.println("byte:	"+by);
+	System.out.println("short:	"+sh);
+	System.out.println("int:	"+a);
+	System.out.println("long:	"+l);
+	System.out.println("float:	"+f);
+	System.out.println("double:	"+dbl);
+	
+	}
+
+}
---
+++ Day2_introduction_to_java/src/com/cg/intro/RunTimeInput.java
@@ -0,0 +1,47 @@
+package com.cg.intro;
+
+import java.util.Scanner;
+
+public class RunTimeInput {
+
+	public static void main(String[] args) {
+		Scanner scr= new Scanner(System.in);
+		//premitive datatype
+		System.out.println("Enter boolean value");
+		boolean b = scr.nextBoolean();
+		//System.out.println("Enter char value");
+		// char c= scr.nextChar();
+		char c= 'a';
+		System.out.println("Enter byte value");
+		byte by = scr.nextByte();
+		System.out.println("Enter short value");
+		short sh = scr.nextShort();
+		System.out.println("Enter int value");
+		int a = scr.nextInt();
+		System.out.println("Enter long value");
+		long l = scr.nextLong();
+		System.out.println("Enter float value");
+		float f = scr.nextFloat();
+		System.out.println("Enter double value");
+		double dbl = scr.nextDouble();
+		// non premitive datatype
+		System.out.println("Enter string value");
+		String str = scr.next();
+		//printing output
+		System.out.println("------------------------------");
+		System.out.println("Data of run time input is");
+		System.out.println("value for boolean is "+b);
+		System.out.println("value for char is "+c);
+		System.out.println("value for byte is "+by);
+		System.out.println("value for short is "+sh);
+		System.out.println("value for int is "+a);
+		System.out.println("value for long is "+l);
+		System.out.println("value for float is "+f);
+		System.out.println("value for double is "+dbl);
+		System.out.println("value for string is "+str);
+		scr.close();
+		
+
+	}
+
+}

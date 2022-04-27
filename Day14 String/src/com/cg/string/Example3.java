5149924aac9fdbb56e3fe9be9e453b10b9c83995
LENOVO <LENOVO@DESKTOP-22RO74R>
2022-04-27 08:44:27 -0400

    first commit

---
+++ Day14 String/src/com/cg/string/Example3.java
@@ -0,0 +1,18 @@
+package com.cg.string;
+
+import java.util.Scanner;
+
+public class Example3 {
+
+	public static void main(String[] args)
+	{
+
+		Scanner s=new Scanner(System.in);
+		String str1=new String(s.nextLine());
+		
+        System.out.println(str1.length());
+        s.close();
+        
+    }
+
+}
---
+++ Day14 String/src/com/cg/string/Example4.java
@@ -0,0 +1,19 @@
+package com.cg.string;
+
+
+public class Example4 {
+
+	public static void main(String[] args) {
+		String str="Shifa";
+		String str1="Afreen";
+		System.out.println(str.charAt(4));
+		System.out.println(str.toLowerCase());
+		System.out.println(str.toUpperCase());
+		System.out.println(str+" "+str1);
+		System.out.println(str+" ".concat(str1));
+		String s = 50+20+50 + "error" +"Anoop"+ 50 + 50 + 20+"Shiwani";  
+		System.out.println(s);
+		
+	}
+
+}

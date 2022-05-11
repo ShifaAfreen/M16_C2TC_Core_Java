776da60d8e958d4b2fbbf4490c8c9155cfa54cb1
LENOVO <LENOVO@DESKTOP-22RO74R>
2022-05-11 06:53:47 -0400

    first commit

---
+++ Day 21_StreamApi/src/com/cg/streamapi/Example2.java
@@ -0,0 +1,18 @@
+package com.cg.streamapi;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		List<Integer>obj=Arrays.asList(10,20,6,30,12,17,6);
+		obj.stream().distinct().forEach(System.out::println);
+		System.out.println("Distinct Output: ");
+		obj.stream().limit(5).forEach(System.out::println);
+		System.out.println("Skip Output: ");
+		obj.stream().skip(1).forEach(System.out::println);
+
+	}
+
+}
